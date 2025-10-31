d0bd54e8bfcd7a7475afc55544304483e9875cbf
Ayomide <oyetimehin31@gmail.com>
2025-10-31 13:32:57 +0100

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/hng/MasterChiefAiAgent/MasterChiefAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hng.MasterChiefAiAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterChiefAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterChiefAiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hng/MasterChiefAiAgent/MasterChiefAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hng.MasterChiefAiAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterChiefAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
